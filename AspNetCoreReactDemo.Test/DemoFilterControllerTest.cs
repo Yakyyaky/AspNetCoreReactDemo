5fed38f7e4047465098135a0e61a473632c2c6e8
jpl.foo@gmail.com <jpl.foo@gmail.com>
2022-05-04 10:50:38 +1200

    added some basic unit testing. Which also shows the weakness of TDD.

---
+++ AspNetCoreReactDemo.Test/DemoFilterControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCoreReactDemo.Controllers;
+using AspNetCoreReactDemo.Extensions;
+using AspNetCoreReactDemo.Model;
+using AspNetCoreReactDemo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
+using Xunit;
+
+namespace AspNetCoreReactDemo.Test
+{
+    public class DemoFilterControllerTest
+    {
+        [Fact]
+        public async Task FailedToSave()
+        {
+            // arrange
+            var data = "some fake data";
+            var storage = new Mock<IDemoModelStorage>();
+            storage.Setup(modelStorage => modelStorage.Update(data)).ReturnsAsync(true);
+            storage.Setup(modelStorage => modelStorage.SaveChanges()).ReturnsAsync(false);
+
+            var problemDetails = new ProblemDetails();
+            var problemFactory = new Mock<ProblemDetailsFactory>();
+            problemFactory
+                .Setup(_ => _.CreateProblemDetails(
+                    It.IsAny<HttpContext>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>())
+                )
+                .Returns(problemDetails).Verifiable();
+
+            var controller = new DemoFilterController(storage.Object)
+            {
+                ProblemDetailsFactory = problemFactory.Object
+            };
+
+            // test
+            var result = await controller.HandleModel(new ModelType()
+            {
+                SomeOtherField = data
+            });
+
+            // Validate
+            Assert.Null(result.Value);
+            Assert.IsType<ObjectResult>(result.Result);
+            problemFactory.Verify();
+        }
+    }
+}
